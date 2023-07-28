32e2304e736303e8d0fb9b7138144de6b56e3833
danielwattimuryjr_ <salamanderttf@gmail.com>
2023-07-28 16:33:31 +0700

    Dashboard OnProgress

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pesanan;
+use App\Models\PesananDetail;
+use App\Models\Payment;
+use App\Models\Menu;
+use App\Models\Kategori;
+
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
     public function index() {
+        $currentMonth = Carbon::now()->format('m');
+        $currentYear = Carbon::now()->format('Y');
+        $today = Carbon::today()->toDateString();
+
         $pageName = "Dashboard";
+
+        $pesanan_tertunda = Pesanan::where('status', 'Belum Selesai')->count();
+
         
-        return view('user.dashboard', compact('pageName'));
+        return view('user.dashboard', compact(
+            'pageName',
+            'pesanan_tertunda'
+        ));
     }
 }
